f5fd7f5d0fc5e7b632e92d386fa6194313710fa5
varshika2306 <varshika425@gmail.com>
2026-07-20 23:24:31 +0530

    Fix PostgreSQL database configuration and deployment setup

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+# Load .env
 env_path = Path(__file__).resolve().parents[2] / ".env"
 
 print("Looking for .env at:", env_path)
@@ -16,15 +17,29 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 print("DATABASE_URL =", DATABASE_URL)
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set in the .env file")
 
+
+
+# Create database engine
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False},
+    )
+else:
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"sslmode": "require"}
+    )
+
+# Database session
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine,
 )
 
+# Base class for models
 Base = declarative_base()
